1588076f14ccf8f77eef3d79dc1f3e3d194d42ff
unknown <komronis01@gmail.com>
2024-02-26 18:09:12 +0600

    'finalversion'

--- init-script.sh
+++ init-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Запустите ваш скрипт generator.py
-python generator.py
+# Скрипт инициализации для создания таблицы bus_data
+psql -U postgres -d bus -c 'CREATE TABLE IF NOT EXISTS bus_data (data JSONB);'
